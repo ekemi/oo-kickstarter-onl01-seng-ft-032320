b6785e37848001a8d78be6d34805d5c56ed179c3
edmundo papi <edmundopierre@yahoo.com>
2020-04-13 00:18:28 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,15 @@
 class Project
-
-  attr_reader :backers,:title
-
-  def initialize(title)
-    @backers = []
-    @title = title
+  class Project
+    attr_reader :title, :backers
+  
+    def initialize(title)
+      @title = title
+      @backers = []
+    end
+  
+    def add_backer(backer)
+      backers << backer
+      backer.backed_projects << self
+    end
+  
   end
-  def add_backer(baker)
-
-    backers << backer
-    backer.backed_projects << self
-
-  end
-end
